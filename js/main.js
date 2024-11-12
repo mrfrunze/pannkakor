4d7f1644e52a22bfa0c93550aaeafaea0c6582d9
Ilya <stoynov.oleg@gmail.com>
2024-11-12 11:24:42 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ ScrollReveal({
 
 ScrollReveal().reveal(".recipe-title", {delay:300, origin: "top"});
 ScrollReveal().reveal(".description", {delay:500, origin: "top"});
-ScrollReveal().reveal(".ingredients-list", {delay:600, origin: "left"});
-ScrollReveal().reveal(".steps-list", {delay:600, origin: "right"});
+ScrollReveal().reveal(".ingredients-list", {delay:300, origin: "left"});
+ScrollReveal().reveal(".steps-list", {delay:300, origin: "right"});
 ScrollReveal().reveal(".fst--elem", {delay:300, origin: "left"});
 ScrollReveal().reveal(".snd--elem", {delay:300, origin: "right"});
 
